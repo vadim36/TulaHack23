bb38e0b2f8ca4bacd64bdb0a75576ad31a86a44e
Vadim Medvedev <vadim130809@gmail.com>
2023-04-28 18:19:51 +0300

    First TulaHack version

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,3 +1,9 @@
-<template></template>
+<template>
+  <div class="card-settings">
+      <h1 class="card-settings__title">Настройки</h1>
+  </div>
+</template>
 <script></script>
-<style></style>
+<style>
+@import "@/assets/style/views/SettingsPage_view.scss";
+</style>
